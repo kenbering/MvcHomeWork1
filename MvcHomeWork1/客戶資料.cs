06911f3b2e74411217251b28154b62f605c45912
kenberlin <kenbering@gmail.com>
2015-04-24 21:49:29 +0800

    Customer e-mail regular expression

--- MvcHomeWork1/客戶資料.cs
+++ MvcHomeWork1/客戶資料.cs
@@ -11,6 +11,7 @@ namespace MvcHomeWork1
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class 客戶資料
     {
@@ -21,11 +22,14 @@ namespace MvcHomeWork1
         }
     
         public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
         public string 客戶名稱 { get; set; }
         public string 統一編號 { get; set; }
         public string 電話 { get; set; }
         public string 傳真 { get; set; }
         public string 地址 { get; set; }
+        [RegularExpression(@"\d{4}-\d{6}")]
         public string Email { get; set; }
     
         public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
