ff0c2f333fc72e23cf7d8df013d1606fd90058e9
kenberlin <kenbering@gmail.com>
2015-04-24 21:40:44 +0800

    Router 錯誤無法導到廠商明細

---
+++ MvcHomeWork1/Controllers/ReportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcHomeWork1.Controllers
+{
+    public class ReportController : Controller
+    {
+        //
+        // GET: /Report/
+
+        public ActionResult Index()
+        {
+            using(var db = new CustomerEntities())
+            {
+                var temp = from p in db.Report_vw
+                           select p;
+                return View(temp.ToList());
+            }
+        }
+
+       
+
+    }
+}
